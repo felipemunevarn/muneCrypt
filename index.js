d904d30ac417986f3a3b8a63f5825b85aacd91e4
andresmunevar@github.com <afmunene@gmail.com>
2022-01-21 16:58:12 -0300

    ending js logic to make functional the app

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("hola");
+let charsToEncrypt = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat'
+}
+
+let charsToDecrypt = {
+    'ai': 'a',
+    'enter': 'e',
+    'imes': 'i',
+    'ober': 'o',
+    'ufat': 'u'
+}
+
+let inputText = document.querySelector(".input");
+let outputText = document.querySelector(".output");
+
+function encrypt(e) {
+    let string = inputText.value;
+    string = string.replace(/[aeiou]/g, m => charsToEncrypt[m]);
+    outputText.value = string;
+    e.preventDefault();
+}
+
+function decrypt(e) {
+    let string = inputText.value;
+    string = string.replace(/ai|enter|imes|ober|ufat/g, m => charsToDecrypt[m]);
+    outputText.value = string;
+    e.preventDefault();
+}
+
+function copy(e) {
+    outputText.select();
+    navigator.clipboard.writeText(outputText.value);
+    e.preventDefault();
+}
+
+let encryptButton = document.querySelector(".encrypt");
+encryptButton.onclick = encrypt;
+
+let decryptButton = document.querySelector(".decrypt");
+decryptButton.onclick = decrypt;
+
+let copyButton = document.querySelector(".copyButton");
+copyButton.onclick = copy;
