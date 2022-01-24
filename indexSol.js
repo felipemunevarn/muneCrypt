654ebc2e7b0034cdb9a669e8032cf37ae538313d
Felipe Munevar <32135161+felipemunevarn@users.noreply.github.com>
2022-01-24 18:48:03 -0300

    extract values from listener

--- indexSol.js
+++ indexSol.js
@@ -1,12 +1,14 @@
-// function validacionCaracteres(){
-    var campoForm = document.querySelector("#textInput");
-    campoForm.addEventListener("input", function(){
-        var expresiones= /[A-Záéíóú]/;
-        var errorTexto = document.querySelector("#error-texto");
-        if(campoForm.value.length > 0 && expresiones.test(campoForm.value)){
-            errorTexto.classList.remove("invisible");
-        }else{
-            errorTexto.classList.add("invisible");
-        }        
-    });
-// }
+var er = false;
+var campoForm = document.querySelector("#textInput");
+campoForm.addEventListener("input", function(){
+    var expresiones= /[A-Záéíóú]/;
+    var errorTexto = document.querySelector("#error-texto");
+    if(campoForm.value.length > 0 && expresiones.test(campoForm.value)){
+        errorTexto.classList.remove("invisible");
+    }else{
+        errorTexto.classList.add("invisible");
+        er = true;
+    }        
+});
+
+console.log(er);
